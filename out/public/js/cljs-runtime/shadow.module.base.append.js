0040b39ec1983816f2c5d1be60c5bd57024a5675
Fay Carsons <faycarsons23@gmail.com>
2023-06-25 14:57:30 -0400

    updating gitignore

--- out/public/js/cljs-runtime/shadow.module.base.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('base');
-
-try { clouds.core.pre_init(); } catch (e) { console.error("An error occurred when calling (clouds.core/pre-init)"); throw(e); }
